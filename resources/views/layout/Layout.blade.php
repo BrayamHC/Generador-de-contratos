0e720f460f445295a8e9f03c46abfbb09a29a837
BrayamHerreraIDT <brayam.herrera@idtconsultores.com>
2025-03-10 11:37:47 -0600

    feat(Layout): Agrega cnd de datagrid.

--- resources/views/layout/Layout.blade.php
+++ resources/views/layout/Layout.blade.php
@@ -4,28 +4,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('titulo', 'Manage Admin')</title>
+    <title>@yield('titulo', 'Signa')</title>
     <!-- SYNCFUSION -->
-    <link rel="stylesheet" href="{{ config('app.url') }}/ej2-dataGrid/styles/material.min.css">
-    <link rel="stylesheet" href="{{ config('app.url') }}/ej2-dataGrid/styles/customized/material.min.css">
-    <script src="{{ config('app.url') }}/ej2-dataGrid/scripts/ej2.min.js" type="text/javascript"></script>
+    <link rel="stylesheet" href="{{ asset('ej2-dataGrid/styles/material.min.css') }}/">
+    <link rel="stylesheet" href="{{ asset('ej2-dataGrid/styles/customized/material.min.css') }}">
+    <script src="{{ asset('ej2-dataGrid/scripts/ej2.min.js') }}" type="text/javascript"></script>
 
     {{-- VUE --}}
     <script src="https://cdn.jsdelivr.net/npm/vue@2"></script>
 
-    <script src="{{ config('app.url') }}/js/bootstrap.js"></script>
+    {{-- DAYJS --}}
+    <script src="https://cdn.jsdelivr.net/npm/dayjs@1/dayjs.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/dayjs@1/locale/es-es.js"></script>
 
     <!-- ESTILOS -->
-    <link rel="stylesheet" href="{{ config('app.url') }}/css/estilos.css" />
+    <link rel="stylesheet" href="{{ asset('css/estilos.css') }}" />
 
     {{-- VERIFICADOR DE CONTRASEÑAS --}}
     <script src="https://cdnjs.cloudflare.com/ajax/libs/zxcvbn/4.4.2/zxcvbn.js"></script>
 
 </head>
 
-<body>
-    @include('components.sidebar', [])
-    <main class="container">
+<body class="layout-extendida">
+    <main class="contenido-general">
         @yield('contenido')
     </main>
 </body>
