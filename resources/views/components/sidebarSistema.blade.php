2fa58c9277eb62670262458baffc7fd9786d9aa6
BrayamHerreraIDT <brayam.herrera@idtconsultores.com>
2025-03-21 14:00:36 -0600

    feat(sidebarSistema): Agrega nuevos botones que redirijen a las nuevas vistas.

--- resources/views/components/sidebarSistema.blade.php
+++ resources/views/components/sidebarSistema.blade.php
@@ -1,17 +1,17 @@
         <!-- Barra lateral -->
         <div class="sidebar">
             <div class="sidebar">
-                <h2>Menú</h2>
+                <!-- <h2 style="color:aliceblue">Menú</h2> -->
                 <img src="{{ asset('images/User.png') }}" alt="Logo" class="logo">
-                <div class="user-info">
-                    <strong>Usuario:</strong> {{ auth()->user()->usuario }}
-                </div>
+                <!-- <div class="user-info">
+                    <strong style="color:aliceblue">Usuario:</strong> {{ auth()->user()->usuario }}
+                </div> -->
                 <button type="button" onclick="location.href='/principal'">Home</button>
                 <button type="button" onclick="location.href='/usuariosGestor'">Usuarios</button>
                 <button type="button" onclick="location.href='/candidatosGestor'">Candidatos</button>
-                <form action="{{ route('logout') }}" method="POST">
-                    @csrf
-                    <button type="submit" id="logout-button">Cerrar sesión</button>
-                </form>
+                <button type="button" onclick="location.href='/empleadosGestor'">RH</button>
+                <button type="button" onclick="location.href='/proyectosGestor'">Proyectos</button>
+                <button type="button" onclick="location.href='/contratosGestor'">Contratos</button>
+                <button type="button" onclick="location.href='/vacacionesGestor'">Vacaciones</button>
             </div>
         </div>
