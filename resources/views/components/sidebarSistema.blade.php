269ce2a919163307f354759879076eed26d83d23
BrayamHerreraIDT <brayam.herrera@idtconsultores.com>
2025-04-03 17:22:14 -0600

    feat(sidebarSistema): Agrega iconos a la sidebar.

--- resources/views/components/sidebarSistema.blade.php
+++ resources/views/components/sidebarSistema.blade.php
@@ -1,17 +1,41 @@
         <!-- Barra lateral -->
         <div class="sidebar">
-            <div class="sidebar">
-                <!-- <h2 style="color:aliceblue">Menú</h2> -->
-                <img src="{{ asset('images/User.png') }}" alt="Logo" class="logo">
-                <!-- <div class="user-info">
-                    <strong style="color:aliceblue">Usuario:</strong> {{ auth()->user()->usuario }}
-                </div> -->
-                <button type="button" onclick="location.href='/principal'">Home</button>
-                <button type="button" onclick="location.href='/usuariosGestor'">Usuarios</button>
-                <button type="button" onclick="location.href='/candidatosGestor'">Candidatos</button>
-                <button type="button" onclick="location.href='/empleadosGestor'">RH</button>
-                <button type="button" onclick="location.href='/proyectosGestor'">Proyectos</button>
-                <button type="button" onclick="location.href='/contratosGestor'">Contratos</button>
-                <button type="button" onclick="location.href='/vacacionesGestor'">Vacaciones</button>
+            <img src="{{ asset('images/User.png') }}" alt="Logo" class="logo">
+            <div class="menus">
+                <div id="homeMenu" title="Home">
+                    <a href="{{ route('principal') }}">
+                        <i class="icon-ol-inicio"></i>
+                    </a>
+                </div>
+                <div id="homeUsuarios" title="Usuarios">
+                    <a href="{{ route('usuarios.listar') }}">
+                        <i class="icon-ol-usuario"></i>
+                    </a>
+                </div>
+                <div id="homeCandidatos" title="Reclutamiento">
+                    <a href="{{ route('candidatos.listar') }}">
+                        <i class="icon-ol-usuarios"></i>
+                    </a>
+                </div>
+                <div id="homeRH" title="RH">
+                    <a href="{{ route('empleados.listar') }}">
+                        <i class="icon-ol-empresa"></i>
+                    </a>
+                </div>
+                <div id="homeProyectos" title="Proyectos">
+                    <a href="{{ route('proyectos.listar') }}">
+                        <i class="icon-ol-reportes"></i>
+                    </a>
+                </div>
+                <div id="homeContratos" title="Contratos">
+                    <a href="{{ route('contratos.listar') }}">
+                        <i class="icon-ol-documento"></i>
+                    </a>
+                </div>
+                <div id="homeVacaciones" title="Vacaciones">
+                    <a href="{{ route('vacaciones.listar') }}">
+                        <i class="icon-ol-calendario"></i>
+                    </a>
+                </div>
             </div>
         </div>
